a9df0921e1006b7959702d02389b4687a6116e7c
Sahil Rajput <sahilrajput03@gmail.com>
2023-09-02 14:52:23 +0530

    Update name of meta tag.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
         <meta charSet="utf-8" />
         <title>My Home Page</title>
         <link rel="canonical" href="http://mysite.com/home" />
-        <meta name="description" content="This is a descripition of the about page"/>
+        <meta name="home page" content="This is a descripition of the home page"/>
       </Helmet>
       <h1>Home Page</h1>
     </div>
